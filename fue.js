492b631e209c742662dd6222435f6019b9a64e36
henriqueFutema <henrique.futema01@gmail.com>
2020-01-15 09:51:16 -0300

    refactor: add Prettier

--- fue.js
+++ fue.js
@@ -1,73 +1,92 @@
-class Fue{
+class Fue {
+  constructor({ el = "#app", data = {}, methods = {} }) {
+    this.el = document.querySelector(el)
+      ? document.querySelector(el)
+      : console.error(`Can not find the element ${el}`);
+    this.data = data;
+    this.methods = methods;
 
-    constructor({el='#app', data={}, methods={}}){
-        this.el = document.querySelector(el) ? document.querySelector(el) : console.error(`Can not find the element ${el}`)
-        this.data = data;
-        this.methods = methods;
-        
-        window.onLoad = this.mapBindText(), this.mapBindAttrs()
-    }
+    (window.onLoad = this.mapBindText()), this.mapBindAttrs();
+  }
 
-    mapBindText(){
-        const textToBind = [...this.el.querySelectorAll('*')]
-        textToBind.map(element =>{
-            const _element = element.innerHTML
-            const data = this.data
-            const text = _element.split(/(\{\{[^}]+\}\})/g).filter(x => x !== '')
-            text.map((_textValue) => _textValue.match(/^\{\{[^}]+\}\}$/) ? binding(_textValue) : _textValue)
-            function binding(textToBind){
-                const _textToBind = textToBind.replace(/^\{\{([^}]+)\}\}$/, '$1').trim()
-                return element.innerHTML = element.innerHTML.replace(textToBind, data[_textToBind] ? data[_textToBind] : textToBind)
-            }       
-        })
-    }
+  mapBindText() {
+    const textToBind = [...this.el.querySelectorAll("*")];
+    textToBind.map(element => {
+      const _element = element.innerHTML;
+      const data = this.data;
+      const text = _element.split(/(\{\{[^}]+\}\})/g).filter(x => x !== "");
+      text.map(_textValue =>
+        _textValue.match(/^\{\{[^}]+\}\}$/) ? binding(_textValue) : _textValue
+      );
+      function binding(textToBind) {
+        const _textToBind = textToBind
+          .replace(/^\{\{([^}]+)\}\}$/, "$1")
+          .trim();
+        return (element.innerHTML = element.innerHTML.replace(
+          textToBind,
+          data[_textToBind] ? data[_textToBind] : textToBind
+        ));
+      }
+    });
+  }
 
-    mapBindAttrs(){
-        const elements = [...this.el.querySelectorAll('*')]
-        const data = this.data
-        elements.map(element => {
-            const attrs = element.attributes
-            for(const v of Object.values(attrs)){
-                if (v.name[0] === '@' || v.name[1] === '-') {
-                    return this.mapDirectives(element, v.name, v.value)
-                }
-
-                const nameAttr = v.name
-                const attrToBind = v.value.split(/(\{\{[^}]+\}\})/g).filter(x => x !== '')
-                attrToBind.map((_attrToBind) => _attrToBind.match(/^\{\{[^}]+\}\}$/) ? bindingAttr(nameAttr, _attrToBind) : _attrToBind)
-            }
-            function bindingAttr(name, value){
-                const _value = value.replace(/^\{\{([^}]+)\}\}$/, '$1').trim()
-                return element.setAttribute(name, data[_value] ? data[_value] : value)
-            }
-        })
-    }
-
-    mapDirectives(el, type, value){
-        const typeDirective = {
-            '@click': () => this.onClick(el, value),
-            'v-model': () => this.vModel(el, value),
-            'default': () => console.error("unknown directive")
+  mapBindAttrs() {
+    const elements = [...this.el.querySelectorAll("*")];
+    const data = this.data;
+    elements.map(element => {
+      const attrs = element.attributes;
+      for (const v of Object.values(attrs)) {
+        if (v.name[0] === "@" || v.name[1] === "-") {
+          return this.mapDirectives(element, v.name, v.value);
         }
-        return (typeDirective[type] || typeDirective['default'])()
-    }
 
-    onClick(el, method){
-        el.addEventListener('click', this.methods[method])
-    }
+        const nameAttr = v.name;
+        const attrToBind = v.value
+          .split(/(\{\{[^}]+\}\})/g)
+          .filter(x => x !== "");
+        attrToBind.map(_attrToBind =>
+          _attrToBind.match(/^\{\{[^}]+\}\}$/)
+            ? bindingAttr(nameAttr, _attrToBind)
+            : _attrToBind
+        );
+      }
+      function bindingAttr(name, value) {
+        const _value = value.replace(/^\{\{([^}]+)\}\}$/, "$1").trim();
+        return element.setAttribute(name, data[_value] ? data[_value] : value);
+      }
+    });
+  }
 
-    vModel(el, data){
-        el.addEventListener('input', ()=>{
-            this.data[data] = el.value
-        })
-    }
+  mapDirectives(el, type, value) {
+    const typeDirective = {
+      "@click": () => this.onClick(el, value),
+      "v-model": () => this.vModel(el, value),
+      default: () => console.error("unknown directive")
+    };
+    return (typeDirective[type] || typeDirective["default"])();
+  }
 
+  onClick(el, method) {
+    el.addEventListener("click", this.methods[method]);
+  }
+
+  vModel(el, data) {
+    el.addEventListener("input", () => {
+      this.data[data] = el.value;
+    });
+  }
 }
 
-Fue.newComponent = function({el='#app', name='', html=`<p>Component ${name} <span>works!</span></p>`, data={}, methods={}}) {
-    const $el = document.querySelector(el)
-    $el.insertAdjacentHTML('beforeend', html)
-    new Fue({el, data, methods})
-}
+Fue.newComponent = function({
+  el = "#app",
+  name = "",
+  html = `<p>Component ${name} <span>works!</span></p>`,
+  data = {},
+  methods = {}
+}) {
+  const $el = document.querySelector(el);
+  $el.insertAdjacentHTML("beforeend", html);
+  new Fue({ el, data, methods });
+};
 
-export { Fue }
+export { Fue };
--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 import { Fue } from "./fue";
 
 const fue = new Fue({
-    el: '#app',
-    data:{
-        name: 'Henrique',
-        count: 1
-    },
-    methods:{
-        click: function(){
-            console.log("aaaaa")
-            meuComponent()
-            console.log(fue.data.count)
-        }
+  el: "#app",
+  data: {
+    name: "Henrique",
+    count: 1
+  },
+  methods: {
+    click: function() {
+      console.log("aaaaa");
+      meuComponent();
+      console.log(fue.data.name);
     }
-})
+  }
+});
 
-const meuComponent = () => Fue.newComponent({ el: '#app', name: 'newComponent', html: '<p>call me by: {{ test }} <span>works!</span></p>', data:{test: 'aaaa'}})
+const meuComponent = () =>
+  Fue.newComponent({
+    el: "#app",
+    name: "newComponent",
+    html: "<p>call me by: {{ test }} <span>works!</span></p>",
+    data: { test: "aaaa" }
+  });
